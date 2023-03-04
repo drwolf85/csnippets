542068bf7a6c37120d6de78d3cc31f097616aa10
Luca <drwolf85@gmail.com>
2023-03-04 13:44:12 -0500

    Documenting average functions

--- src/averages.c
+++ src/averages.c
@@ -5,6 +5,14 @@
 
 /* Arithmetic averages */
 
+/**
+ * It computes the arithmetic average of the elements of an array, ignoring NaN values
+ * 
+ * @param x the array of values
+ * @param n the number of elements in the array
+ * 
+ * @return The arithmetic average of the non-NaN values in the array.
+ */
 double arithmetic_average(double *x, size_t n) {
     size_t i;
     double res = 0.0;
@@ -19,6 +27,13 @@ double arithmetic_average(double *x, size_t n) {
     return res / nnan;
 }
 
+/**
+ * It calculates the arithmetic average of a set of numbers.
+ * 
+ * @param avg the average value (previously computed)
+ * @param n the number of samples (previously used)
+ * @param x the new value to be added to the average
+ */
 void arithmetic_average_online(double *avg, size_t *n, double x) {
     double tmp;
     if (*n > 0) {
@@ -34,6 +49,14 @@ void arithmetic_average_online(double *avg, size_t *n, double x) {
 
 /* Geometric averages */
 
+/**
+ * It computes the geometric average of an array of doubles, ignoring NaN values
+ * 
+ * @param x the array of numbers
+ * @param n the number of elements in the array
+ * 
+ * @return The geometric average of the array `x`.
+ */
 double geometric_average(double *x, size_t n) {
     size_t i;
     double res = 1.0;
@@ -48,6 +71,17 @@ double geometric_average(double *x, size_t n) {
     return pow(res, 1.0 / nnan);
 }
 
+/**
+ * "For each element in the array, if it's not NaN, add one to the number of non-NaN elements, 
+ * and add the log of the element to the sum of the logs of the elements."
+ * 
+ * The first thing to notice is that the function is almost identical to the previous one.
+ * 
+ * @param x the array of numbers
+ * @param n the number of elements in the array
+ * 
+ * @return The geometric mean of the array `x`.
+ */
 double geometric_average2(double *x, size_t n) {
     size_t i;
     double res = 0.0;
@@ -62,6 +96,13 @@ double geometric_average2(double *x, size_t n) {
     return exp(res / nnan);
 }
 
+/**
+ * It calculates the geometric average of a set of numbers.
+ * 
+ * @param avg the current average (previoulsy computed)
+ * @param n the number of samples seen so far
+ * @param x the new value to be added to the average
+ */
 void geometric_average_online(double *avg, size_t *n, double x) {
     double tmp;
     if (*n > 0) {
@@ -80,6 +121,14 @@ void geometric_average_online(double *avg, size_t *n, double x) {
 
 /* Harmonic averages */
 
+/**
+ * It computes the harmonic average of the elements of an array, ignoring NaN values
+ * 
+ * @param x the array of values
+ * @param n the number of elements in the array
+ * 
+ * @return The harmonic mean of the array.
+ */
 double harmonic_average(double *x, size_t n) {
     size_t i;
     double res = 1.0;
@@ -94,6 +143,13 @@ double harmonic_average(double *x, size_t n) {
     return nnan / res;
 }
 
+/**
+ * It calculates the harmonic average of a set of numbers.
+ * 
+ * @param avg the current average (previously computed)
+ * @param n the number of samples (previously used)
+ * @param x the new value to be "added" to the average
+ */
 void harmonic_average_online(double *avg, size_t *n, double x) {
     double tmp;
     if (*n > 0) {
@@ -112,6 +168,18 @@ void harmonic_average_online(double *avg, size_t *n, double x) {
 
 /* Generalized averages */
 
+/**
+ * It computes the generalized average of the elements of an array, using a 
+ * function `fun` and its inverse (i.e., `invfun`). This function ignores 
+ * the NaN values
+ *
+ * @param x the array of values
+ * @param n the number of elements in the array
+ * @param fun a function that takes a double and returns a double
+ * @param invfun the inverse of the function to be applied to the data
+ * 
+ * @return The average of the function applied to the elements of the array.
+ */
 double generalized_average(double *x, size_t n, double (*fun)(double), double (*invfun)(double)) {
     size_t i;
     double res = 0.0;
@@ -126,6 +194,15 @@ double generalized_average(double *x, size_t n, double (*fun)(double), double (*
     return (*invfun)(res / nnan);
 }
 
+/**
+ * It takes a running average of a function of a variable
+ * 
+ * @param avg the average value (previously computed)
+ * @param n the number of samples seen so far
+ * @param x the new value to be added to the average
+ * @param fun a function that takes a double and returns a double
+ * @param invfun the inverse of the function you want to use
+ */
 void generalized_average_online(double *avg, size_t *n, double x, double (*fun)(double), double (*invfun)(double)) {
     double tmp;
     if (*n > 0) {
