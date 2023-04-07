1911711885d715f835f7f2a4bf8cdefa8df4e314
Luca <drwolf85@gmail.com>
2023-04-07 01:13:04 -0400

    Fixing bugs on multivariate ECDF

--- src/mvecdf.c
+++ src/mvecdf.c
@@ -24,7 +24,7 @@ typedef struct {
  * 
  * @return the difference between the two values
  */
-int cmp_values(void *aa, void *bb) {
+int cmp_values(const void *aa, const void *bb) {
     values *a = (values *)aa;
     values *b = (values *)bb;
     if (fabs(a->v - b->v) < EPS_TOLL) {
@@ -39,7 +39,6 @@ int cmp_values(void *aa, void *bb) {
 
 /**
  * It sorts the columns of a matrix, 
- * finds the minimum index among the columns of the matrix, 
  * and then computes the cumulative distribution function
  * 
  * @param pr pointer to the output array (a vector of length `n`)
@@ -72,25 +71,17 @@ size_t * mvecdf(double *pr, double *x, size_t n, size_t p) {
             qsort(&v[j * n], n, sizeof(values), cmp_values);
         /* Set indexes in each column of an "order" matrix */
         #pragma omp parallel for private(i, j) collapse(2)
-        for (j = 1; j < p; j++) {
+        for (j = 0; j < p; j++) {
             for (i = 0; i < n; i++) {
                 o[n * j + v[n * j + i].i] = i;
             }
         }
-        /* Find the minimum index among the columns of the matrix */
         #pragma omp parallel for private(i, j)
         for (i = 0; i < n; i++) {
-            for (j = 1; j < p; j++) {
-                if (o[i] > o[n * j + i]) {
-                    o[i] = o[n * j + i];
-                }
+            pr[i] = 1.0;
+            for (j = 0; j < p; j++) {
+                pr[i] *= (0.5 + (double) o[n * j + i]) * invn;
             }
-        }
-        /* Compute cumulative distribution function */
-        #pragma omp parallel for private(i)
-        for (i = 0; i < n; i++) {
-            pr[i] = 0.5 + (double) o[i];
-            pr[i] *= invn;
         }
         /* Prepaaring the data for a returning pointer of size `n` */
         v = (values *) realloc(v, n * sizeof(values));
@@ -111,3 +102,81 @@ size_t * mvecdf(double *pr, double *x, size_t n, size_t p) {
     free(v);
     return o;
 }
+
+/**
+ * It sorts the columns of a matrix, 
+ * finds the minimum index among the columns of the matrix, 
+ * and then computes the cumulative distribution function
+ * 
+ * @param pr pointer to the output array (a vector of length `n`)
+ * @param x a matrix of size `n` x `p`
+ * @param n number of rows (or samples)
+ * @param p number of columns in the matrix (or variables)
+ */
+void mvecdf_prob(double *pr, double *x, int *_n, int *_p) {
+    size_t i, j;
+    size_t n = (size_t) *_n;
+    size_t p = (size_t) *_p;
+    double const invn = 1.0 / (double) n;
+    values *v;
+    size_t *o;
+
+    v = (values *) malloc(n * p * sizeof(values));
+    o = (size_t *) malloc(n * p * sizeof(size_t));
+    if (v && o) {
+        /* Organize the data of a matrix in a stracture values */
+        // #pragma omp parallel for private(i, j) collapse(2)
+        for (j = 0; j < p; j++) {
+            for (i = 0; i < n; i++) {
+                v[n * j + i].v =  x[n * j + i];
+                v[n * j + i].i = i;
+            }
+        }
+        /* Sorting the data in each column of the matrix */
+        #pragma omp parallel for private(j)
+        for (j = 0; j < p; j++)
+            qsort(&v[j * n], n, sizeof(values), cmp_values);
+        /* Set indexes in each column of an "order" matrix */
+        #pragma omp parallel for private(i, j) collapse(2)
+        for (j = 0; j < p; j++) {
+            for (i = 0; i < n; i++) {
+                o[n * j + v[n * j + i].i] = i;
+            }
+        }
+        /* Compute the empirical cumulative distribution function */
+        #pragma omp parallel for private(i, j)
+        for (i = 0; i < n; i++) {
+            pr[i] = 1.0;
+            for (j = 0; j < p; j++) {
+                pr[i] *= (0.5 + (double) o[n * j + i]) * invn;
+            }
+        }
+    }
+    free(v);
+    free(o);
+}
+
+// dyn.load("test.so")
+// pr <- double(n <- 500L)
+// for (p in 1L + seq_len(7L)) {
+//     x <- matrix(rnorm(n * p), n, p)
+//     pr <- .C("mvecdf_prob", pr = pr, x, n, p, DUP = FALSE)$pr
+//     cat("Number of variables:", p, "\n")
+//     print(range(pr))
+//     print(x[which.min(pr),])
+//     print(x[which.max(pr),])
+// }
+// p <- 2L
+// x <- matrix(rnorm(n * p), n, p)
+// pr <- .C("mvecdf_prob", pr = pr, x, n, p, DUP = FALSE)$pr
+// par(mfrow=c(2, 2))
+// plot(x, cex = exp(pr - mean(pr) + 1) - 1)
+// plot(x, type = "n")    
+// text(x, labels = round(pr, 3))
+// th <- apply(pnorm(x), 1, prod)
+// plot(th, pr, xlim = 0:1, ylim = 0:1); abline(0:1, col=8)
+// p <- 8L
+// x <- matrix(rnorm(n * p), n, p)
+// pr <- .C("mvecdf_prob", pr = pr, x, n, p, DUP = FALSE)$pr
+// th <- apply(pnorm(x), 1, prod)
+// plot(th, pr); abline(0:1, col=8)
