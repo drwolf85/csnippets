9373538a28db5a7db22cc654c1c5972eef9eeee4
Luca <drwolf85@gmail.com>
2023-05-04 22:54:26 -0400

    Including comments to `bernstein_smoother.c`

--- src/models/bernstein_smoother.c
+++ src/models/bernstein_smoother.c
@@ -8,6 +8,20 @@
 #include <math.h>
 #include <omp.h>
 
+/**
+ * The function calculates the Bernstein polynomial of a given degree for a given function.
+ * 
+ * @param x x is a double precision floating point number that represents the input value for the
+ * Bernstein polynomial. It should be between 0 and 1, inclusive.
+ * @param f The parameter `f` is a pointer to a function that takes a double as input and returns a
+ * double as output. This function is used to evaluate the function being approximated by the Bernstein
+ * polynomial.
+ * @param n n is the degree of the Bernstein polynomial. It determines the number of terms in the
+ * polynomial and the accuracy of the approximation.
+ * 
+ * @return The function `bernstein_poly` returns a double value which is the result of evaluating the
+ * Bernstein polynomial of degree `n` at the point `x` using the function `f` as the input function.
+ */
 double bernstein_poly(double x, double (*f)(double), int n) {
     int i;
     double omx, tmp, res = 0.0;
@@ -34,6 +48,20 @@ double bernstein_poly(double x, double (*f)(double), int n) {
     return res;
 }
 
+/**
+ * The function implements the Bernstein smoothing algorithm for a set of values using a given
+ * parameter.
+ * 
+ * @param x The input value to be smoothed, which should be between 0 and 1 inclusive.
+ * @param v v is a pointer to an array of doubles representing the values of a function at a set of
+ * points. The function is being smoothed using the Bernstein polynomial method.
+ * @param n The degree of the Bernstein polynomial, which needs to be equal to the number of control
+ * points.
+ * 
+ * @return The function `bernstein_smooth` returns a double value which is the result of applying the
+ * Bernstein polynomial smoothing function to the input value `x` using the coefficients in the array
+ * `v` of length `n`.
+ */
 double bernstein_smooth(double x, double *v, int n) {
     int i;
     double omx, tmp, res = 0.0;
