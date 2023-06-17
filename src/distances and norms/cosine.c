a6dbc8938903193cd0cb984c00c9ac14f3bf7dc9
Luca <drwolf85@gmail.com>
2023-06-17 11:33:14 -0400

    General updates

---
+++ src/distances and norms/cosine.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <omp.h>
+
+#define MIN_EPS 1e-20
+
+/**
+ * The function calculates the cosine distance between two vectors of double values.
+ * 
+ * @param x A pointer to an array of double values representing the first vector.
+ * @param y The parameter `y` is a pointer to a double array containing the second vector for which the
+ * cosine distance is being calculated.
+ * @param n The parameter "n" represents the size of the arrays "x" and "y", which are the input arrays
+ * containing the vectors for which the cosine distance is being calculated.
+ * 
+ * @return the cosine distance between two vectors represented by arrays of double values.
+ */
+double cosine_distance(double *x, double *y, size_t n) {
+    size_t i;
+    double xn = 0.0, yn = 0.0, rs = 0.0;
+
+    #pragma omp parallel for private(i) reduction(+ : xn, yn, rs)
+    for (i = 0; i < n; i++) {
+        xn += x[i] * x[i];
+        yn += y[i] * y[i];
+        rs += x[i] * y[i];
+    }
+    xn = (xn > MIN_EPS) * xn + (xn <= MIN_EPS) * MIN_EPS;
+    yn = (yn > MIN_EPS) * yn + (yn <= MIN_EPS) * MIN_EPS;
+    xn *= yn;
+    return sqrt(2.0 - 2.0 * rs / xn);
+}
+
+/* Test function */
+int main() {
+    double x[] = {1.0, 2.0, 0.5, -0.5, -2.0};
+    double y[] = {-1.1, 1.9, 0.4, -0.4, 1.0};
+    double cd = cosine_distance(x, y, 5);
+    printf("Computed cosine distance is %f\n", cd);
+    return 0;
+}
+
---
+++ src/distances and norms/hamming.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <omp.h>
+
+/**
+ * The function calculates the Hamming distance between two strings of characters using parallel
+ * processing.
+ * 
+ * @param x The parameter x is a pointer to a character array representing the first string for which
+ * we want to calculate the Hamming distance.
+ * @param y The parameter "y" is a pointer to a character array, which is used as input to the
+ * hamming_distance function. It is likely that this array represents a binary string or bit sequence.
+ * @param n The parameter `n` represents the length of the input strings `x` and `y`.
+ * 
+ * @return The function `hamming_distance` returns the Hamming distance between two strings `x` and `y`
+ * of length `n`. The Hamming distance is the number of positions at which the corresponding symbols
+ * are different in the two strings.
+ */
+uint64_t hamming_distance(char *x, char *y, size_t n) {
+    size_t i;
+    char tmp, k;
+    uint64_t res = 0;
+    #pragma omp parallel for private(i, tmp, k) reduction(+ : res)
+    for (i = 0; i < n; i++) {
+        tmp = x[i] ^ y[i];
+        for (k = 0; k < 8; k++)
+            res += (uint64_t) ((tmp >> k) & 1);
+    }
+    return res;
+}
+
+/* Test function */
+int main() {
+    char x[] = "This is a test!";
+    char y[] = "This is a nest.";
+    uint64_t hd = hamming_distance(x, y, 15);
+    printf("Computed Hamming distance is %lu\n", hd);
+    return 0;
+}
