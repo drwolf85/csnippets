07f53ea80672e7f0bbc47c637bb221066bf92cbc
Luca <drwolf85@gmail.com>
2023-07-09 20:30:08 -0400

    Inner and outer products of a generic matrix

--- src/linear algebra/outer_prod_matrix.c
+++ src/linear algebra/outer_prod_matrix.c
@@ -1,25 +1,25 @@
 #include <omp.h>
 
 /**
- * @brief Compute the inner product of a generic matrix X of size (n x p)
+ * @brief Compute the outer product of a generic matrix X of size (n x p)
  * 
- * @param res_m a pointer to the symmetric matrix (p x p) in output
+ * @param res_m a pointer to the symmetric matrix (n x n) in output
  * @param x a pointer to the input matrix with data (stored as column-major)
  * @param n a pointer to the number of rows of `x`
  * @param p a pointer to the number of columns of `x`
  */
-void inner_prod_matrix(double *res_m, double *x, int *n, int *p) {
+void outer_prod_matrix(double *res_m, double *x, int *n, int *p) {
     int i, j, k;
     double tmp;
     #pragma omp parallel for simd private(j, tmp, k)
-    for (i = 0; i < *p; i++) {
-        for (j = i; j < *p; j++) {
+    for (i = 0; i < *n; i++) {
+        for (j = i; j < *n; j++) {
             tmp = 0.0;
-            for (k = 0; k < *n; k++) {
+            for (k = 0; k < *p; k++) {
                 tmp += x[*n * i + k] * x[*n * j * + k];
             }
-            res_m[*p * i + j] = tmp; 
-            res_m[*p * j + i] = tmp; 
+            res_m[*n * i + j] = tmp; 
+            res_m[*n * j + i] = tmp; 
         }
     }
 }
