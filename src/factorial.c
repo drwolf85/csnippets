2b91c933666e494e8a99100a9a0d2a95784b74d3
Luca Sartore <drwolf85@gmail.com>
2023-11-04 23:41:11 -0400

    Including Gosper approximation in  `factorial.c`

--- src/factorial.c
+++ src/factorial.c
@@ -5,3 +5,15 @@ double factorial(double x) {
     double res = lgamma(x + 1.0);
     return exp(res);
 }
+
+double stirling(double x) {
+    double res = x * log(x) - x;
+    res += 0.5 * log(2.0 * M_PI * x); 
+    return exp(res);
+}
+
+double gosper(double x) { 
+    double res = x * log(x) - x;
+    res += 0.5 * log(M_PI * (2.0 * x + 1.0 / 3.0)); 
+    return exp(res);
+}
