4356f7c60e2cdf3dfcdd18a39f0bc0975ce5836f
Luca Sartore <drwolf85@gmail.com>
2023-11-10 14:16:30 -0500

    Updating multivariate kernels

--- src/models/kernel for smoothing/multivariate.c
+++ src/models/kernel for smoothing/multivariate.c
@@ -28,8 +28,9 @@ double additive_1_bounded_kernel(double *x, int p, double (*K)(double)) {
     double tmp, res = 0.0;
     for (i = 0; i < p; i++) {
         tmp = K(x[i]);
-        for (j = 0; j < i; j++) res *= ID_BOUND_1_STRICT(x[j]);
-        for (j++; j < p; j++) res *= ID_BOUND_1_STRICT(x[j]);
+        for (j = 0; j < i; j++) tmp *= ID_BOUND_1_STRICT(x[j]);
+        for (j++; j < p; j++) tmp *= ID_BOUND_1_STRICT(x[j]);
+        res += tmp;
     }
     return res / ((double) p * pow(2.0, (double) (p - 1)));
 }
@@ -39,8 +40,9 @@ double product_p_bounded_kernels(double *x, int p, double (**K)(double)) {
     double tmp, res = 0.0;
     for (i = 0; i < p; i++) {
         tmp = K[i](x[i]);
-        for (j = 0; j < i; j++) res *= ID_BOUND_1_STRICT(x[j]);
-        for (j++; j < p; j++) res *= ID_BOUND_1_STRICT(x[j]);
+        for (j = 0; j < i; j++) tmp *= ID_BOUND_1_STRICT(x[j]);
+        for (j++; j < p; j++) tmp *= ID_BOUND_1_STRICT(x[j]);
+        res += tmp;
     }
     return res / ((double) p * pow(2.0, (double) (p - 1)));
 }
