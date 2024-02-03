8063b507b5309336b4dfeb486d775fd319b7dc1d
Luca Sartore <drwolf85@users.noreply.github.com>
2024-02-03 14:44:16 -0500

    Including and editing MPEC algorithms  for linear objective functions

--- src/operation research/binary optim/mpec_admm_lin.c
+++ src/operation research/binary optim/mpec_admm_lin.c
@@ -4,11 +4,12 @@
 
 #define DEFAULT_RHO 0.8
 #define DEFAULT_SIGMA 1.01
-#define MAX_ITER 1000
+#define DEFAULT_ALPHA 1.0
+#define MAX_ITER 100
 #define MY_EPS_TOLL 1e-9
 
 /**
- * @brief Mathematical Programming with Equilibrium Constraints - Exact Penalty Method
+ * @brief Mathematical Programming with Equilibrium Constraints - Alternating Direction Method of Multipliers
  * 
  * @param x A pointer to a vector of unknown values to optimize in {-1, 1}^n
  * @param c A pointer to a known vector of cost (use to compute a linear objective function)
@@ -20,9 +21,9 @@
  * @param sigma Scaling parameter operating on `rho`
  * @param T Number of void iterations on the updates of `rho` 
  */
-void mpec_epm_lin(double *x, double *c, size_t n, double *A, double *b, size_t p, double rho, double sigma, size_t T) {
+void mpec_admm_lin(double *x, double *c, size_t n, double *A, double *b, size_t p, double rho, double sigma, double alpha, size_t T) {
     size_t i, j, t = 1;
-    double sr, inprd;
+    double sr, dec, inprd;
     double L = fabs(*c);
 
     double *e = NULL;
@@ -31,6 +32,7 @@ void mpec_epm_lin(double *x, double *c, size_t n, double *A, double *b, size_t p
 
     rho = (double)(rho > 0.0) * rho + (double)(rho <= 0.0) * DEFAULT_RHO;
     sigma = (double)(sigma > 1.0) * sigma + (double)(sigma <= 1.0) * DEFAULT_SIGMA;
+    alpha = (double)(alpha > 1.0) * alpha + (double)(alpha <= 1.0) * DEFAULT_ALPHA;
     for (i = 1; i < n; i++) L += (double) (fabs(c[i]) > L) * (fabs(c[i]) - L);
     L *= 2.0;
 
@@ -39,8 +41,8 @@ void mpec_epm_lin(double *x, double *c, size_t n, double *A, double *b, size_t p
         memset(x, 0, sizeof(double) * n); /* Initialize values of x*/
         while(t <= MAX_ITER) { /** FIXME: there are other conditions to consider...*/
             /* Primal step */
-            /* c^T x + rho * (n - x^T v) : x \in [-1, 1] \cap constraint set... which is equivalent to*/
-            inprd = (double) t / (double) T;
+            /* c^T x + rho * (n - x^T v) + 0.5 * alpha * (n - x^T v)^2: x \in [-1, 1] \cap constraint set... which is equivalent to*/
+            dec = (double) t / (double) T;
             if (e) {                
                 for (j  = 0; j < p; j++) {
                 /* constraint: A %*% x <= b */
@@ -51,27 +53,31 @@ void mpec_epm_lin(double *x, double *c, size_t n, double *A, double *b, size_t p
                     e[j] -= b[j];
                     e[j] *= L * (double) (e[j] > 0.0);
                 }
-                /* (c - rho * v)^T x + rho * n : x \in [-1, 1] \cap constraint set...*/
+                /* v^T x */
+                inprd = 0.0;
+                for (i = 0; i < n; i++)
+                    inprd += x[i] * v[i];
+                /* (c - rho * v)^T x + rho * n + 0.5 * alpha * (n - v^T x)^2: x \in [-1, 1] \cap constraint set...*/
                 for (i = 0; i < n; i++) {
                     sr = x[i] * x[i];
                     g[i] = 0.0;
                     for (j  = 0; j < p; j++) {
                         g[i] += A[n * j + i] * e[j]; /** NOTE: this is column-major format */ 
                     }
-                    g[i] += c[i] - rho * v[i];
+                    g[i] += c[i] - (rho + alpha * ((double) n - inprd)) * v[i];
                     g[i] *= 1.0 - sr;
-                    x[i] = tanh(atanh(x[i]) - inprd * g[i]);
+                    x[i] = tanh(atanh(x[i]) - dec * g[i]);
                 }
             } else {
-                /* (c - rho * v)^T x + rho * n : x \in [-1, 1] \cap constraint set...*/
+                /* (c - rho * v)^T x + rho * n + 0.5 * alpha * (n - x^T v)^2: x \in [-1, 1] \cap constraint set...*/
                 for (i = 0; i < n; i++) {
                     sr = x[i] * x[i];
-                    g[i] = c[i] - rho * v[i];
+                    g[i] = c[i] - (rho + alpha * ((double) n - inprd)) * v[i];
                     g[i] *= 1.0 - sr;
-                    x[i] = tanh(atanh(x[i]) - inprd * g[i]);
+                    x[i] = tanh(atanh(x[i]) - dec * g[i]);
                 }
             }
-            /* Dual step */
+            /* Dual step: use solution form EPM as an approximation */
             inprd = 0.0;
             for (i = 0; i < n; i++)
                 inprd += x[i] * x[i];
@@ -81,8 +87,9 @@ void mpec_epm_lin(double *x, double *c, size_t n, double *A, double *b, size_t p
                     v[i] = x[i] * inprd;
             }
             /* Updating penalty parameter */
-            sr = sigma * rho;
-            rho += (double) !(t % T) * ((L + (double)(L > sr) * (sr - L)) - rho);
+            rho += alpha * (n - inprd);
+            sr = (sigma - 1.0) * alpha;
+            alpha += (double) !(t % T) * sr;
             t++;
         }
     }
@@ -100,18 +107,18 @@ void main() {
     double c[] = {-3.0, 1.0, -0.2, 1.2, 3.0};
     double A[] = {1.0, 1.0, 0.0, 0.0, 0.0, \
                   0.0, 0.0, 1.0, 1.0, 1.0};
-    double b[] = {0.01, -1.9};
+    double b[] = {0.01, -0.9};
     // double b[] = {0.01, -0.9};
     int const p = 2;
     int i;
 
     printf("Binary optim without linear constraints:\n");
-    mpec_epm_lin(x, c, N, 0, 0, 0, -1.0, -1.0, 15);
+    mpec_admm_lin(x, c, N, 0, 0, 0, -1.0, -1.0, -1.0, 15);
     for (i = 0; i < N; i++)
         printf("x[%d] = %f -> %d\n", i, x[i], (int) (x[i] >= 0.0));
 
     printf("\nBinary optim with linear constraints:\n");
-    mpec_epm_lin(x, c, N, A, b, p, -1.0, -1.0, 15);
+    mpec_admm_lin(x, c, N, A, b, p, -1.0, -1.0, -1.0, 15);
     for (i = 0; i < N; i++)
         printf("x[%d] = %f -> %d\n", i, x[i], (int) (x[i] >= 0.0));
 }
