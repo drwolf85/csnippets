521d5385a73e05f187afa50d67a95936d525cfab
Luca Sartore <drwolf85@gmail.com>
2024-08-31 22:00:14 -0400

    Updated Taylor and Mac Laurin Series

--- src/approximations/taylor_series.c
+++ src/approximations/taylor_series.c
@@ -20,6 +20,21 @@ double deriv(double x0, double h, double htype, double(*f)(double), unsigned p, 
     }
 }
 
+double * mac_laurin_expand(unsigned p, double (*f)(double)) {
+	unsigned i;
+	double *coef = (double *) calloc((p + 1), sizeof(double));
+	double den = 1.0;
+	if (coef) {
+		coef[0] = f(0.0) / den;
+		for (i = 1; i <= p; i++) {
+			coef[i] = deriv(0.0, 1e-2, 0.5, f, i, 1.0);
+			den *= (double) i;
+			coef[i] /= den;
+		}
+	}
+	return coef;
+}
+
 double * taylor_expand(double x0, unsigned p, double (*f)(double)) {
 	unsigned i;
 	double *coef = (double *) calloc((p + 1), sizeof(double));
@@ -39,12 +54,23 @@ double * taylor_expand(double x0, unsigned p, double (*f)(double)) {
 int main() {
     unsigned i;
     unsigned const power = 4;
-    double *cvec = taylor_expand(sqrt(M_PI) * 0.5, power, exp);
+    double const x_0 = sqrt(M_PI) * 0.5;
+    double *cvec = mac_laurin_expand(power, exp);
+    if (cvec) {
+        printf("The polynom based on Mac Laurin approximation is:\n");
+        printf("%.3f ", cvec[0]);
+        for (i = 1; i <= power; i++) {
+            printf("+ %.3f * x^%d ", cvec[i], i);
+        }
+        printf("\n");
+    }    
+    free(cvec);
+    cvec = taylor_expand(x_0, power, exp);
     if (cvec) {
         printf("The polynom based on Taylor approximation is:\n");
-        printf("%f ", cvec[0]);
+        printf("%.3f ", cvec[0]);
         for (i = 1; i <= power; i++) {
-            printf("+ %f x^%d ", cvec[i], i);
+            printf("+ %.3f * (x - %.3f)^%d ", cvec[i], x_0, i);
         }
         printf("\n");
     }
