9857c4f5e2e00ef933759925d38f9fa29b1b131f
Luca Sartore <drwolf85@gmail.com>
2025-02-15 16:25:02 -0500

    Including EPP defined by De Finetti

--- src/erpp.c
+++ src/erpp.c
@@ -4,6 +4,28 @@
 #include <omp.h>
 
 /**
+ * @brief Empirical Rectangular Prism Probability
+ *
+ * @param X Pointer to a matrix of data (stored in column-major format)
+ * @param dimX Pointer to a vector with the dimensions of the matrix `X`
+ * @param lb Pointer to a vector of lower bounds for the rectangular prims
+ * @param ub Pointer to a vector of upper bounds for the rectangular prims
+ * @return double
+ */
+double erpp(double *X, int *dimX, double *lb, double *ub) {
+	unsigned i, cnt = 0;
+	#pragma omp parallel for simd reduction(+ : cnt)
+	for (i = 0; i < dimX[0]; i++) {
+		char tmp = 1;
+		for (unsigned j = 0; j < dimX[1] && tmp; j++) {
+			tmp &= (char) (lb[j] < X[dimX[0] * j + i] && X[dimX[0] * j + i] <= ub[j]);
+		}
+		cnt += (unsigned) tmp;
+	}
+	return (double) cnt / (double) *dimX;
+}
+
+/**
  * @brief Empirical Closed Rectangular Prism Probability
  *
  * @param X Pointer to a matrix of data (stored in column-major format)
@@ -54,6 +76,7 @@ int main() {
 	int dimx[] = {5, 2};
 	double lb [] = {0.2, 0.3};
 	double ub [] = {0.4, 0.8};
+	printf("Emp. RPP: %g\n", erpp(xmat, dimx, lb, ub));
 	printf("Emp.Closed RPP: %g\n", ecrpp(xmat, dimx, lb, ub));
 	printf("Emp.Open RPP: %g\n", eorpp(xmat, dimx, lb, ub));
 	return 0;
